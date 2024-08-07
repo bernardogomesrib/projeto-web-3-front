2f0c0aa47192c5a0c72cd4e891202b48dc0b7e25
Bernardo Gomes <bernardogomesrib@gmail.com>
2024-08-07 11:42:53 -0300

    a

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex min-h-screen w-full flex-col items-center justify-between">
+        <main className="flex min-h-screen w-[95vw] flex-col items-center justify-between ms-8">
           {children}
         </main>
       </body>
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,46 @@
-
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div>
+    <div className="w-[99vw] flex flex-wrap gap-2 items-start">
       
+      <Card className="w-full md:w-[60vw] gap-3 flex flex-wrap ">
+        <CardHeader className="text-4xl font-bold w-full ">
+        <h1 className="">Boards</h1>
+        </CardHeader>
+        <CardContent className="w-full flex flex-wrap gap-2 justify-evenly">
+        <Button className="w-[45%]">Hardware</Button>
+        <Button className="w-[45%]">Eletrônica</Button>
+        <Button className="w-[45%]">Games</Button>
+        <Button className="w-[45%]">Programação</Button>
+        <Button className="w-[45%]">S.O.</Button>
+        <Button className="w-[45%]">I.A.</Button>
+        </CardContent>
+        
+        <CardFooter className="flex flex-wrap">
+          <h1 className="text-4xl font-bold w-full ">Threads populares</h1>
+          <div className="w-full md:w-[30%] flex flex-wrap">
+            <Button className="w-full">
+              Games
+            </Button>
+            <Image src="/games.png" alt="icone" width={136} height={136}/>
+          </div>
+        </CardFooter>
+      </Card>
+      <Card className="w-full md:w-[35vw] flex flex-wrap p-6">
+        <h2 className="font-bold w-full md:w-[35vw] ">Últimas publicações</h2>
+        <Card className="w-full flex flex-wrap p-3">
+          <CardTitle className="flex"><Image src="/next.svg" alt="next Logo" width={72} height={16} />Games retro</CardTitle>
+          <CardContent>
+            <p>mario kart é over rated.</p>
+          </CardContent>
+
+        </Card>
+      </Card>
+     
+
     </div>
   );
 }
+
