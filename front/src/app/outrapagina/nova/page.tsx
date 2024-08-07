b1837b698be1a1c5966678f625324edee6072396
Bernardo Gomes <bernardogomesrib@gmail.com>
2024-08-07 09:59:51 -0300

    inicio

---
+++ front/src/app/outrapagina/nova/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Outra Página dentro da outra rota</h1>
+    </div>
+  );
+}
---
+++ front/src/app/outrapagina/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Page() {
+    return (
+        <div className='hover:bg-gray-700 bg-blue-600 w-[350px]'>
+            <h1>Outra Página</h1>
+            <div className="flex min-h-screen justify-between items-end h-full">
+                <div>Item 1</div>
+                <div>Item 2</div>
+                <div>Item 3</div>
+            </div>
+        </div>
+    );
+}
