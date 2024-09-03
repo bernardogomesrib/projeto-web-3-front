ebe95bd8d8c24947c8cf19ae6c2cd57fa79de9bc
Bernardo Gomes <bernardogomesrib@gmail.com>
2024-09-02 21:15:19 -0300

    agora funciona a pagina de thread mas não tá 100% falta criar um thread e fazer o get da api

--- front/src/app/[boardid]/[threadid]/page.tsx
+++ front/src/app/[boardid]/[threadid]/page.tsx
@@ -3,7 +3,8 @@ import { Button } from '@/components/ui/button';
 import { Card, CardFooter } from '@/components/ui/card';
 import Image from 'next/image';
 import { notFound, redirect } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { ResponseCard } from './responseCard';
 
 const boardsExistentes = [
   "hw",
@@ -24,83 +25,77 @@ const threadsExistentes = [
 ]
 
 
-const respostas = [{  
-      id:1,
-      mensagem:"mensage",
-      arquivo:"icondialog.png",
-      ip:"123.123.123"
-    },
-    {
-      id:2,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:"45.77.88.55"
-    },
-    {
-      id:3,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-      },
-    {
-      id:4,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-    },
-    {
-      id:5,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-    },
-    {
-      id:6,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-    },
-    {
-      id:7,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-    },
-    {
-      id:8,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-    },
-    {
-      id:9,
-      mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
-      arquivo:'SO.png',
-      ip:""
-    }
-    
-  
+const respostasRecebidas = [{
+  id: 1,
+  mensagem: "mensage",
+  arquivo: "/icondialog.png",
+  ip: "123.123.123"
+},
+{
+  id: 2,
+  mensagem: "Losexo voluntário id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '',
+  ip: "45.77.88.55"
+},
+{
+  id: 3,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/games.png',
+  ip: ""
+},
+{
+  id: 4,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/IA.png',
+  ip: ""
+},
+{
+  id: 5,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/vercel.svg',
+  ip: ""
+},
+{
+  id: 6,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/errorImage.png',
+  ip: ""
+},
+{
+  id: 7,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/SO.png',
+  ip: ""
+},
+{
+  id: 8,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/SO.png',
+  ip: ""
+},
+{
+  id: 9,
+  mensagem: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cumque voluptate id, laborum et itaque explicabo doloremque fugiat ipsa deserunt nesciunt culpa, saepe doloribus voluptatem commodi tempora vel sequi animi.",
+  arquivo: '/SO.png',
+  ip: ""
+}
+
+
 ]
-type size = {
-  width:number,
-  height:number
+
+
+class Resposta {
+  constructor(public resp: any) {}
+  getCard() {
+    console.log("fui chamado!");
+  }
 }
-export type res ={
-  resposta:any,
-  imageSize:size|null,
-  imageExpanded:boolean
-}
-function expand(boolean:boolean){
-  boolean = !boolean;
-}
-function handleLoad(resp:res,e:any){
-  resp.imageSize!.width = e.target.naturalWidth;
-  resp.imageSize!.height = e.target.naturalHeight;
-}
-export default function ThreadPage({ params }: { params: { boardid: string,threadid:string } }) {
-  const boardid = params.boardid;
-  const threadid = params.threadid;
-  const thread =  {
+
+
+
+export default function ThreadPage({ params }: { params: { boardid: string, threadid: string } }) {
+  const { boardid, threadid } = params;
+  const thread = {
     id: 1,
     titulo: "como lidar com isto?",
     mensagem: "fui comprar um notebook só pra ficar em casa como servidor ou substituir o meu notebook antigo, mas daí quando perguntei sobre o notebook que estava a venda a mulher me manda uma dessas, eu achei que fosse sempre uma piadada internet, não uma realidade :(",
@@ -110,72 +105,65 @@ export default function ThreadPage({ params }: { params: { boardid: string,threa
     createdAt: "2024-09-01T20:13:03.000Z",
     updatedAt: "2024-09-01T21:03:34.000Z",
     boardId: "elt",
-    userId: null
-  }
-  
-  //useState<any|null>();
-  const answers = useState<any[]>([]);
-  const usouUmavez = React.useRef(false);
-  const [expandThreadImage,setExpandThreadImage] = useState(false);
+    userId: null,
+  };
+
+  const [respostas, setRespostas] = useState<Resposta[]>([]);
+  const [expandThreadImage, setExpandThreadImage] = useState(false);
   const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
-  const [respostas,setResposta] = useState<res[]|null>(null)
-  const [respostasImageSize,setRespostasImageSize] = useState<any[]>([{}])
-  
-  const handleImageLoad = (e:any) => {
+  const usouUmaVez = React.useRef(false);
+
+  useEffect(() => {
+    if (!usouUmaVez.current) {
+      setRespostas(respostasRecebidas.map((r) => new Resposta(r)));
+      usouUmaVez.current = true;
+    }
+  }, []);
+
+  const handleImageLoad = (e: any) => {
     setImageSize({
       width: e.target.naturalWidth,
       height: e.target.naturalHeight,
     });
   };
-  const handleImageClick= ()=>{
+
+  const handleImageClick = () => {
     setExpandThreadImage(!expandThreadImage);
+  };
+
+  if (!boardsExistentes.includes(boardid)) {
+    return notFound();
   }
-  React.useEffect(() => {
-    async function requests() {
-      
-    }
-    if (!usouUmavez.current) {
-      console.log("Board ID: ", boardid);
-      console.log("Thread ID: ", threadid);
 
-
-      usouUmavez.current = true;
-    }
-  });
-
-  if (boardsExistentes.includes(boardid)) {
-
-    if (threadsExistentes.includes(threadid)) {
-      return (
-        <div className="w-[99vw] flex flex-wrap gap-2 items-start justify-center">
-          <Card className='m-1'>
-            <div className="pl-5">{thread.id} - {thread.userId?thread.userId:"Anonymous"}</div>
-            <h4 className='pl-6'>{thread.titulo}</h4>
-            <div className='flex p-6 justify-left'>
-              <div className='flex-wrap flex'>
-              <p className='text-xs'>{imageSize.width}x{imageSize.height} <Button className="p-0 text-decoration:underline h-5 bg-transparent" onClick={handleImageClick}>{expandThreadImage?("Diminuir imagem"):("Expandir imagem")}</Button></p>
-              {!expandThreadImage?(<Image width={150} height={150} src={thread.arquivo} alt={thread.titulo} onClick={handleImageClick} onLoad={handleImageLoad}/>):(<Image width={imageSize.width} height={imageSize.height} src={thread.arquivo} alt={thread.titulo} onClick={handleImageClick} onLoad={handleImageLoad}/>)}
-              </div>
-              <p className='p-6'>{thread.mensagem}</p>
-
-            </div>
-            <CardFooter>
-              {/* {respostas.map((resposta)=>{
-                <Card key={resposta.id}>
-                    
-                </Card>
-              })} */}
-            </CardFooter>
-          </Card>
-        </div>
-      );
-
-    } else {
-
-      return redirect(notFound());
-    }
-  } else {
-
+  if (!threadsExistentes.includes(threadid)) {
     return redirect(notFound());
   }
+
+  return (
+    <div className="w-[99vw] flex flex-wrap gap-2 items-start justify-center">
+      <Card className="m-1 text-[var(--font-color)]">
+        <div className="pl-5">{thread.id} - {thread.userId ? thread.userId : "Anonymous"}</div>
+        <h4 className="pl-6">{thread.titulo}</h4>
+        <div className="flex p-6 justify-left">
+          <div className="flex-wrap flex">
+            <p className="text-xs">{imageSize.width}x{imageSize.height} <Button className="p-0 text-decoration:underline h-5 bg-transparent" onClick={handleImageClick}>{expandThreadImage ? "Diminuir imagem" : "Expandir imagem"}</Button></p>
+            {!expandThreadImage && thread.arquivo ? (
+              <Image width={150} height={150} src={thread.arquivo} alt={thread.titulo} onClick={handleImageClick} onLoad={handleImageLoad} />
+            ) : (
+              <Image width={imageSize.width} height={imageSize.height} src={thread.arquivo} alt={thread.titulo} onClick={handleImageClick} onLoad={handleImageLoad} />
+            )}
+          </div>
+          <p className="p-6">{thread.mensagem}</p>
+        </div>
+        <CardFooter className='flex flex-wrap'>
+        <Button className="fixed right-0 top-1/2 transform -translate-y-1/2 ">
+          Responder thread
+        </Button>
+          {respostas.map((r, index) => (
+            <ResponseCard key={index} id={`${index}cardid`} index={index} resp={r.resp} />
+          ))}
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
---
+++ front/src/components/dialogs/newAnswer.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+// import Link from "next/link"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { sendNewThread } from "@/lib/threads";
+import jwt from 'jsonwebtoken';
+import { useState } from "react";
+
+
+export default function NewThread({ Trigger }: { Trigger: string }) {
+    let aux = "" as string | null;
+    try {
+        aux = localStorage.getItem('user');
+    }
+    catch (e) {
+        console.log("para de dar erro demonio");
+    }
+
+    const logic = (aux) ? (aux) : ('unkonwn')
+    const [user, setUser] = useState<any>(jwt.decode(logic));
+
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const local = window.location.href.split('/')[3];
+        console.log({formData: formData, local: local, user: user});
+        const answer = await sendNewThread(formData, local);
+        console.log(answer);
+    };
+    
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className={Trigger}>Criar thread</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[80%] max-h-[90%] overflow-y-auto">
+                <DialogHeader>
+                    <DialogTitle className="text-center py-2">Nova thread</DialogTitle>
+                    <hr className="border-t border-white" />
+                </DialogHeader>
+                <div className="w-fit flex gap-3 pt-4">
+                    <div>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>user</AvatarFallback>
+                        </Avatar>
+                    </div>
+                    <div className="pt-1"><strong>{user?.nome !== undefined ? user.nome : 'Anonimo'}</strong></div>
+                </div>
+                <form onSubmit={handleSubmit} className="grid gap-4 py-4">
+                    <div className="grid gap-2">
+                        <Input
+                            type="file"
+                            id="file-upload"
+                            name="image"
+                            style={{ width: '32%' }}
+                        />
+                    </div>
+                    <div className="grid gap-2">
+                        <Textarea
+                            name="mensagem"
+                            rows={7}
+                            className="col-span-3 h-50"
+                            placeholder="Comentário..."
+                            style={{ resize: "none" }}
+                        ></Textarea>
+                    </div>
+                    <div className="text-center mt-4">
+                        <Button className="bg-[#BF32DC] px-10 text-sm" type="submit" style={{
+                            textShadow: `
+                            -1px -1px 0 #000,
+                            1px -1px 0 #000,
+                            -1px 1px 0 #000,
+                            1px 1px 0 #000`
+                        }}>Publicar</Button>
+                    </div>
+                </form>
+
+            </DialogContent>
+        </Dialog>
+    )
+}
